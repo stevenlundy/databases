2dd9a00f5041a4a2b52c1efa9157ebbd6f4fa5d8
stevenlundy <steven.lundy@gmail.com>
2015-09-11 16:25:17 -0700

    Implement draft functionality to get message data from database and craft code to connect to database

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,30 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    // a function which produces all the messages
+    get: function (callback) {
+      db.connect();
+      var queryString = "SELECT u.name AS username, m.message AS text, r.name AS roomname, m.id AS objectId, m.timestamp AS createdAt \
+                        FROM messages m JOIN users u ON (m.user=u.id) JOIN rooms r ON (m.room=r.id)";
+      var queryArgs = [];
+
+      db.query(queryString, queryArgs, function(err, results){
+        db.end();
+        if(err){
+          console.log(err);
+        } else {
+          callback(results);
+        }
+      });
+    }, 
+    // a function which can be used to insert a message into the database
+    post: function () {
+      db.connect();
+
+
+
+      db.end();
+    } 
   },
 
   users: {
